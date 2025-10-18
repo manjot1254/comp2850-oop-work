17936ffa574f19a676d460c08788b8725da90d75
manjot1254 <vvvt1316@leeds.ac.uk>
2025-10-18 23:22:24 +0000

    7.3

--- tasks/task7_3_2/MutableList.kt
+++ tasks/task7_3_2/MutableList.kt
@@ -1,6 +1,6 @@
 // Task 7.3.1: list element access
 fun main(){
-    val numbers = listOf(9, 3, 6, 2, 8, 5)
+    val numbers = mutableListOf(9, 3, 6, 2, 8, 5)
     println(numbers)
     println(numbers[0])
     println(numbers.get(0))
@@ -9,7 +9,20 @@ fun main(){
     println(numbers.first())
     println(numbers.last())
 
+    val numbers2 = listOf(2,3,4)
+
     numbers.add(1)
+    println(numbers)
+    numbers.addAll(numbers2)
+    println(numbers)
+    numbers.remove(4)
+    println(numbers)
+    numbers.removeAll(numbers2)
+    println(numbers)
+    numbers.removeAt(0)
+    println(numbers)
+    numbers.clear()
+    println(numbers)
 
     //val empty = listOf("")
     //println(empty.first())
