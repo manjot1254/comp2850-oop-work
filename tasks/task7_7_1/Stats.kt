e2c6a6aada0f07eacfe35e452bda5a39b9b944b6
manjot1254 <vvvt1316@leeds.ac.uk>
2025-10-19 19:40:12 +0000

    7.7 started

--- tasks/task7_7_1/Stats.kt
+++ tasks/task7_7_1/Stats.kt
@@ -1,1 +1,13 @@
 // Task 7.7.1: stats for a numeric dataset
+fun readData() {
+    // Print a prompt for the user
+    println("Enter a series of floating-point numbers")
+    val floatList = readln().toInt()
+    var outputList = [""]
+    
+    // Write a loop to read the numbers
+    for (item in floatList){
+        // Inside this loop, call add() to add a number to list
+        outputList.add(floatList[item])
+    }
+}
